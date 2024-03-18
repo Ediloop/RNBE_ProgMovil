f9fddcc085dbd73568df964f19ef21cf8fd7c0b5
Ediloop <100537142+Ediloop@users.noreply.github.com>
2024-03-18 15:26:53 -0600

    Create NationalLowSeason.kt

---
+++ 1Parcial/Act4_HerClasAbseInt/Clases/NationalLowSeason.kt
@@ -0,0 +1,24 @@
+package Clases
+
+import Cancelable
+import Descuento
+
+class NationalLowSeason(ciudad: String) : Nacional(ciudad), Descuento, Cancelable {
+
+    override val descuento = 15 // porcentaje (15%)
+    override val tipoDescuento = 0 // 0 para porcentaje, 1 para cantidad
+
+    override fun obtenerPrecio(numDias: Int): Double {
+        val monto = super.obtenerPrecio(numDias)
+        return if (monto == 0.0) 0.0 else obtenerPrecioConDescuento(monto).toDouble()
+    }
+
+    override fun cancelar(): String {
+        if (!reservado) {
+            return "No se ha realizado ninguna reserva para cancelar."
+        }
+        reservado = false
+        montoPagado = 0.0
+        return "¡Viaje cancelado exitosamente!"
+    }
+}
